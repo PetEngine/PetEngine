dab1deffe24909b918d8452f5d3e01d4293d5d27
Roman-Skabin <skabin.roman@mail.ru>
2023-05-06 23:08:50 +0200

    Fixed pre-commit hook

--- pre_commit_hook.bat
+++ pre_commit_hook.bat
@@ -1,3 +1,6 @@
-@echo off
-git diff --cached --name-status | findstr /S /I /C:"@DoNotCommit" *.jai
-if %errorLevel% equ 0 exit /B 1
+"C:\Program Files\Git\bin\git.exe" diff --cached --name-status | findstr /S /I /C:"@DoNotCommit" *.jai
+if %errorLevel% equ 0 (
+    exit /B 1
+) else (
+    exit /B 0
+)
