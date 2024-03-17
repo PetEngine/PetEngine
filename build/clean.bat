dccfa910866c4d521ce9f2109e4a7256e2fed361
Roman-Skabin <rskabin13@gmail.com>
2024-03-17 16:56:11 +0100

    Added vtune folder to cleanup.bat and .gitignore

--- build/clean.bat
+++ build/clean.bat
@@ -3,8 +3,9 @@
 setlocal enableExtensions
 
 pushd %~dp0%
-    if exist "..\output"        rmdir /S /Q "..\output"
     if exist "..\intermediates" rmdir /S /Q "..\intermediates"
+    if exist "..\output"        rmdir /S /Q "..\output"
+    if exist "..\vtune"         rmdir /S /Q "..\vtune"
     if exist ".\.build"         rmdir /S /Q ".\.build"
 popd
 
