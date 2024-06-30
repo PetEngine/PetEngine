669f23494475a1d86e6b957d5ed4ca12bc00564c
Roman-Skabin <rskabin13@gmail.com>
2024-06-30 21:00:04 +0200

    Added runtime meshes to clean script

--- build/clean.bat
+++ build/clean.bat
@@ -1,13 +1,13 @@
 @echo off
-
+pushd %~dp0%
 setlocal enableExtensions
 
-pushd %~dp0%
-    if exist "..\intermediates" rmdir /S /Q "..\intermediates"
-    if exist "..\output"        rmdir /S /Q "..\output"
-    if exist "..\vtune"         rmdir /S /Q "..\vtune"
-    if exist ".\.build"         rmdir /S /Q ".\.build"
-popd
+if exist "..\intermediates"                rmdir /S /Q "..\intermediates"
+if exist "..\output"                       rmdir /S /Q "..\output"
+if exist "..\vtune"                        rmdir /S /Q "..\vtune"
+if exist ".\.build"                        rmdir /S /Q ".\.build"
+if exist "..\source\assets\meshes\runtime" rmdir /S /Q "..\source\assets\meshes\runtime"
 
-exit /B %errorLevel%
 endlocal
+popd
+exit /B %errorLevel%
