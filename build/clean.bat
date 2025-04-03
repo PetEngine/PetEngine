5c4d375f01a72aebd3a0037aa8a63d457ee49e0b
Roman-Skabin <rskabin13@gmail.com>
2025-04-03 19:16:31 +0200

    Embedding manifest without mt.exe
    Also move DPI awareness setting to manifest

--- build/clean.bat
+++ build/clean.bat
@@ -7,7 +7,6 @@ if exist "..\output"                       rmdir /S /Q "..\output"
 if exist "..\vtune"                        rmdir /S /Q "..\vtune"
 if exist ".\.build"                        rmdir /S /Q ".\.build"
 if exist "..\source\assets\meshes\runtime" rmdir /S /Q "..\source\assets\meshes\runtime"
-if exist ".\windows.manifest"              del /AH ".\windows.manifest"
 
 endlocal
 popd
