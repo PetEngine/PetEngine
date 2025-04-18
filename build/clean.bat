c395f71e0727df72bd070c03bc72e83ad6531f45
Roman-Skabin <rskabin13@gmail.com>
2025-04-18 19:53:27 +0200

    Initial dumb way to convert textures

--- build/clean.bat
+++ build/clean.bat
@@ -2,11 +2,12 @@
 pushd %~dp0%
 setlocal enableExtensions
 
-if exist "..\intermediates"                rmdir /S /Q "..\intermediates"
-if exist "..\output"                       rmdir /S /Q "..\output"
-if exist "..\vtune"                        rmdir /S /Q "..\vtune"
-if exist ".\.build"                        rmdir /S /Q ".\.build"
-if exist "..\source\assets\meshes\runtime" rmdir /S /Q "..\source\assets\meshes\runtime"
+if exist "..\intermediates"                  rmdir /S /Q "..\intermediates"
+if exist "..\output"                         rmdir /S /Q "..\output"
+if exist "..\vtune"                          rmdir /S /Q "..\vtune"
+if exist ".\.build"                          rmdir /S /Q ".\.build"
+if exist "..\source\assets\meshes\runtime"   rmdir /S /Q "..\source\assets\meshes\runtime"
+if exist "..\source\assets\textures\runtime" rmdir /S /Q "..\source\assets\textures\runtime"
 
 endlocal
 popd
